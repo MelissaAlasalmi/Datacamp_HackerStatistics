a441120e593156ef06d283ca96911f744d7cd700
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2021-01-16 12:17:29 +0200

    init commit

---
+++ diceRoll.py
@@ -0,0 +1,19 @@
+import numpy as numpy
+
+# Which step you are on in a building
+step = 50
+
+# Roll a simulated dice
+dice = np.random.randint(6, 7)
+print("dice roll: " + str(dice))
+
+# factor the roll into outcome
+if dice <= 2 :
+    step = step - 1
+elif dice >= 3 and dice <= 5 :
+    step = step + 1
+else :
+    reroll = np.random.randint(1, 7)
+    step = step + reroll 
+    print("reroll: " + str(reroll))
+print("steps after round: " + str(step))
